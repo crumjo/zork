051e8b7a0e7e08b8a67889e6711ffb7ddb800b0c
Patton M Finley <finleyp@mail.gvsu.edu>
2017-11-20 00:28:17 -0500

    Fixed if statements

--- zork.py
+++ zork.py
@@ -122,34 +122,27 @@ class Player(object):
             i = i - 1
 
     def p_attack(self, p_monster, p_weapon):
-        print("weapon", p_weapon.name)
-        print("monster", p_monster.name)
         if p_weapon.name == "hershey kiss":
-            print("1")
             p_monster.health = p_monster.health - (p_weapon.attack_mod * self.attack)
             monster.update()
         elif p_weapon.name == "sour straw":
-            print("2")
-            if p_monster == "Zombie":
-                print("3")
+            if p_monster.name == "Zombie":
                 p_monster.health = p_monster.health - 2 * (p_weapon.attack_mod * self.attack)
                 monster.update()
-            elif p_monster == "Werewolf":
-                print("4")
+            elif p_monster.name == "Werewolf":
                 monster.update()
             else:
-                print("5")
                 p_monster.health = p_monster.health - (p_weapon.attack_mod * self.attack)
                 monster.update()
         elif p_weapon.name == "choclate bar":
-            if p_monster == "Vampire" or p_monster == "Werewolf":
+            if p_monster.name == "Vampire" or p_monster.name == "Werewolf":
                 monster.update()
             else:
                 p_monster.health = p_monster.health - (p_weapon.attack_mod * self.attack)
                 monster.update()
         else:
             print("6")
-            if p_monster == "Ghoul":
+            if p_monster.name == "Ghoul":
                 p_monster.health = p_monster.health - 5 * (p_weapon.attack_mod * self.attack)
                 monster.update()
             else:
